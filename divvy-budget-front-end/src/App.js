afc6c4bffcd97c20580c0f80d4ba247605cb6c1c
Brady Berryessa <brady.berryessa@gmail.com>
2019-03-06 18:07:34 -0700

    Started working on new category modal

--- divvy-budget-front-end/src/App.js
+++ divvy-budget-front-end/src/App.js
@@ -1,18 +1,39 @@
 import "./App.css";
 
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "./components/button";
+import Modal from "./components/modal";
 
 const App = () => {
+  const [showNewCategoryModal, setNewCategoryModal] = useState(true);
+
   const createNewCategory = () => {
-    console.log("creating new category");
+    setNewCategoryModal(true);
+    console.log(showNewCategoryModal);
+  };
+
+  const cancelCategory = () => {
+    console.log("cancelCategory");
+  };
+
+  const saveNewCategory = () => {
+    console.log("saveNewCategory");
   };
 
   return (
     <div className="App">
       <div>Testing this out;</div>
       <Button buttonClicked={createNewCategory}>+ Create new category</Button>
+      <Modal show={showNewCategoryModal}>
+        <h2>New Category</h2>
+        <div style={{ display: "flex", flexDirection: "row" }}>
+          <input />
+          <input />
+        </div>
+        <Button buttonClicked={cancelCategory}>Cancel</Button>
+        <Button buttonClicked={saveNewCategory}>Save</Button>
+      </Modal>
     </div>
   );
 };
